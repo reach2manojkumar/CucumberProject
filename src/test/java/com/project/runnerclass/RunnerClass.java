fb0e6a5359c334f75f25f04b98c92753ffbe38ab
reach2manojkumar <reach2manoj506@gmail.com>
2022-07-31 22:35:53 +0530

    new code

--- src/test/java/com/project/runnerclass/RunnerClass.java
+++ src/test/java/com/project/runnerclass/RunnerClass.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/FeaturesFile",glue="com.project.stepdefinition",monochrome = true,dryRun = false,plugin = { "pretty","html:target/cucumber-reports" },publish=true)
+@CucumberOptions(features="src/test/resources/FeaturesFile",glue="com.project.stepdefinition",monochrome = true,dryRun = false,plugin = {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"},publish=true)
 public class RunnerClass {
 
 }
